35f488bcf373116448dc7e84dfba16faa5566686
MatthewQuilapio <156325023+MatthewQuilapio@users.noreply.github.com>
2025-08-13 23:49:33 +0800

    Add files via upload

---
+++ Buy_and_Sell_land/login_page.py
@@ -0,0 +1,75 @@
+import tkinter as tk
+from window_config import window_config
+from tkinter import messagebox
+from sql_database import connection
+from dashboard import main_function
+from create_acc import fill_up_acc
+
+
+conn = connection()
+cursor = conn.cursor()
+
+login_page = tk.Tk()
+login_page.title("Login")
+button_font_style = ("Arial", 12, "bold")
+
+x,y = window_config(300, 250, login_page)
+login_page.geometry(f'{300}x{250}+{x}+{y}')
+login_page.configure(bg = 'indigo')
+login_page.resizable(False, False)
+
+"""def hide_pass():
+    password_entry.config(show='*')
+    checkbutton.config(command=show_pass)
+def show_pass():
+    password_entry.config(show='')
+    checkbutton.config(command=hide_pass)"""
+select_table_acc = f"""
+SELECT * from lands_acc_list where username_var = %s and password_var = %s;
+"""
+
+conn = connection()
+cursor = conn.cursor()
+
+def confirm_acc(user_entry, pass_entry):
+    for_login = (user_entry.get(), pass_entry.get())
+    cursor.execute(select_table_acc, for_login)
+    acc_result = cursor.fetchall()
+    #print(acc_result)
+    #[(1, 'Quilapio', 'Matthew', 'Leia', '09669771623', 'quilapiomatthew31@gmail.com', 'wa7la8zu')]
+    list_acc = [item for temp in acc_result for item in temp]
+    #print(list_acc)
+    #name_result = [i for i in acc_result]
+    #pass_result = [i for i in acc_result]
+    
+    #print(id_result)
+    #conn.commit()
+    if (user_entry.get() in list_acc) and (pass_entry.get() in list_acc):
+        login_page.destroy()
+        main_function(list_acc)
+        
+        
+        #messagebox.showinfo("Login", "You're in")
+
+fillup_frame = tk.Frame(login_page, width=500, background="#5D3A9B")
+fillup_frame.pack(padx=5, pady=5, side="top", fill="both", expand=True)
+button_frame = tk.Frame(login_page, width=500, background="#5D3A9B")
+button_frame.pack(padx=5, pady=5, side="top", fill="both", expand=True)
+
+tk.Label(fillup_frame, text="UserName:").pack(anchor="w", pady=(0,2)) 
+username_entry = tk.Entry(fillup_frame)
+username_entry.pack(fill="x", pady=(10, 10), padx=(10, 10))
+
+tk.Label(fillup_frame, text="Password:").pack(anchor="w", pady=(0,2)) 
+password_entry = tk.Entry(fillup_frame, show='*')
+password_entry.pack(fill="x", pady=(10, 10), padx=(10, 10))
+
+#checkbutton = tk.Checkbutton(login_page, text="Show Password", command=show_pass,onvalue=1, offvalue=0).pack()
+                             
+
+tk.Button(button_frame, font=button_font_style, text="Confirm", command = lambda:confirm_acc(username_entry, password_entry)).pack(side='left')
+tk.Button(button_frame, font=button_font_style, text="Create New Account", command= lambda:fill_up_acc(login_page)).pack(side='left')
+
+
+
+login_page.mainloop()
